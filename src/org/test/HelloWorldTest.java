b7c0813b7f682258a5802e124b9fa97cb0d82edb
George <fed. georg @ yandex dot ru>
2022-06-30 08:25:52 +0300

    Just for a test

---
+++ src/org/test/HelloWorldTest.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public class HelloWorldTest {
+    private static final String greetings = "Hello";
+
+    public static void main(String[] args) {
+        System.out.println(greetings);
+    }
+}
